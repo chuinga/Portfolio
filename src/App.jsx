a6c143ad0ee0f26b4b3f2928e347da33dcf791ec
chuinga <mfproencamartins@gmail.com>
2024-02-23 11:16:48 +0000

    Defining components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Header from './components/header';
+import HomePage from './pages/HomePage';
+import Sidebar from './components/sidebar';
+import Footer from './components/footer';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <Header />
+    <div>
+      <HomePage />
+      <Sidebar />
+    </div>
+    <Footer />
     </>
   )
 }
