9ef32bf94d3d7c50c2cfc83a31b2ad3495c08fdc
chuinga <mfproencamartins@gmail.com>
2024-02-25 17:59:00 +0000

    Adding translation functionalities

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
+// Import translation
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+// Import styling
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <I18nextProvider i18n={i18next}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </I18nextProvider>    
   </React.StrictMode>,
 )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,49 @@
+// Import translation
+import { useTranslation } from "react-i18next";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import translationEN from '../locales/en/translation.json';
+import translationES from '../locales/es/translation.json';
+import translationFR from '../locales/fr/translation.json';
+import translationPT from '../locales/pt/translation.json';
 // Import styling
 import "../styles/homePage.css";
 
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  es: {
+    translation: translationES,
+  },
+  fr: {
+    translation: translationFR,
+  },
+  pt: {
+    translation: translationPT,
+  },  
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
 function body() {
+  const { t } = useTranslation();
+
   return (
     <>
       <div>
-        <p className='blue'>Hi, my name is</p>
+        <p className='blue'>{t("greeting")}</p>
+        <h2 className='lightBlue'>Miguel Martins</h2>
+        <h2>{t("my_title")}</h2>
+        <p>{t("my_description")}</p>
+        {/* <p className='blue'>Hi, my name is</p>
         <h2 className='lightBlue'>Miguel Martins</h2>
         <h2>I create stuff for the web...</h2>
         <p>
@@ -13,7 +51,7 @@ function body() {
           about coding, downhill biking, and composing new tunes. I'm driven by
           a hunger for knowledge and I consistently strive to learn and improve
           with great attention to detail.
-        </p>
+        </p> */}
       </div>
     </>
   );
