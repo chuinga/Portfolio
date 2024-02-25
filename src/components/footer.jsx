7f12277e452512bee78507819114aeade1b3213b
chuinga <mfproencamartins@gmail.com>
2024-02-25 18:48:14 +0000

    Implementing language functionality

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,18 +1,31 @@
+// Import React, useState and useTranslation hook
+import React, { useState } from "react";
+import { useTranslation } from 'react-i18next';
 // Import images
 import En from '../assets/en.png';
 import Es from '../assets/es.png';
 import Fr from '../assets/fr.png';
 import Pt from '../assets/pt.png';
-// Import Styles
+// Import styles
 import '../styles/footer.css'
 
 function footer() {
+    const { i18n } = useTranslation();
+    const [selectedLanguage, setSelectedLanguage] = useState('');
+    
+    const handleLanguageChange = (newLang) => {
+        i18n.changeLanguage(newLang);
+        setSelectedLanguage(newLang);
+    }
+
+    const isSelected = (lang) => selectedLanguage === lang;
+
     return ( 
         <div className='lang'>
-            <img src={En} alt="En" />
-            <img src={Es} alt="Es" />
-            <img src={Fr} alt="Fr" />
-            <img src={Pt} alt="Pt" />     
+            <img src={En} alt="English" className={`langIcon ${isSelected('en') ? 'selected' : ''}`} onClick={() => handleLanguageChange('en')} />
+            <img src={Es} alt="Español" className={`langIcon ${isSelected('es') ? 'selected' : ''}`} onClick={() => handleLanguageChange('es')} />
+            <img src={Fr} alt="Français" className={`langIcon ${isSelected('fr') ? 'selected' : ''}`} onClick={() => handleLanguageChange('fr')} />
+            <img src={Pt} alt="Português" className={`langIcon ${isSelected('pt') ? 'selected' : ''}`} onClick={() => handleLanguageChange('pt')} />     
         </div>
 );        
 }
