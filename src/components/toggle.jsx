bd3eda213282f454468798cb0a93260807b627db
chuinga <mfproencamartins@gmail.com>
2024-03-13 18:57:25 +0000

    fixing light/dark mode toggle issue

--- src/components/toggle.jsx
+++ src/components/toggle.jsx
@@ -18,3 +18,23 @@ const Toggle = ({ handleChange, isChecked }) => {
 }
  
 export default Toggle;
+/* import React from 'react';
+// Import styles
+import '../styles/toggle.css';
+
+const Toggle = ({ handleChange, isChecked }) => {
+    return ( 
+        <div className='toggleContainer'>
+            <input 
+                type="checkbox" 
+                id='check' 
+                className='toggle' 
+                onChange={handleChange} 
+                checked={isChecked} 
+            />
+            <label htmlFor="check"></label>
+        </div>        
+     );
+}
+ 
+export default Toggle; */
