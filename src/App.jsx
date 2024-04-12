dab528fd792fbb354fc09b4db525df6edbf87091
chuinga <mfproencamartins@gmail.com>
2024-04-12 12:20:43 +0100

    feat-iframe functionality to display project site embedded within react app

--- src/App.jsx
+++ src/App.jsx
@@ -22,26 +22,33 @@ function App() {
 
   return (
     <div className='outerContainer'>
-      <div className='mainContainer' data-theme={isDark ? 'dark' : 'light'}>     
-        <header>
-          <Header />
-        </header> 
-        <main>
-          <div className='innerContainer'>
-            <Routes>
-              <Route path='/' element={<HomePage />} />
-              <Route path='/project/:projectName' element={<Project />} />      
-              <Route path='*' element={<PageNotFound />} />  
-            </Routes>      
-            <Sidebar />
-          </div>
-        </main>
-        <footer>
-          <Footer isDark={isDark} toggleDarkMode={() => setIsDark(!isDark)} />
-        </footer>       
+      <div className='mainContainer' data-theme={isDark ? 'dark' : 'light'}> 
+        
+        <div className='Content'>
+          <header>
+            <Header />
+          </header> 
+          <main>
+            <div className='innerContainer'>
+              <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/project/:projectName' element={<Project />} /> 
+                // Clean up the code by replacing the following routes with a single dynamic route.:
+                <Route path='/project/gravity/embed' element={<iframe src="https://gravitygame.netlify.app/" title="Gravity" style={{width: '100%', height: '100%'}}></iframe>} />
+                <Route path='/project/pawsitivehomes/embed' element={<iframe src="https://main--pawsitivehomes.netlify.app/" title="Pawsitive Homes" style={{width: '100%', height: '100%'}}></iframe>} />
+                <Route path='/project/groovegrid/embed' element={<iframe src="https://groovegrid.netlify.app/" title="GrooveGrid" style={{width: '100%', height: '100%'}}></iframe>} />
+                <Route path='*' element={<PageNotFound />} />  
+              </Routes>      
+              <Sidebar />
+            </div>
+          </main>
+          <footer>
+            <Footer isDark={isDark} toggleDarkMode={() => setIsDark(!isDark)} />
+          </footer> 
+        </div>  
+              
       </div>  
-    </div>
-    
+    </div>    
   )
 }
 
